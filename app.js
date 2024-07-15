e25aae9b32c1fda461216fd2908345e76fd5bbdd
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-16 00:35:19 +0700

    Update app.js Rev02 shortening

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 let ectjob = {};
-while(true){
 let key = prompt("กรุณาใส่ key หรือพิมพ์ stop");
-if (key.toLowerCase() === 'stop') {
-  break;
-}
+while(key.toLowerCase() !== 'stop'){
 let value = prompt(`กรุณาใส่ value ของ ${key}`);
 ectjob[key] = value;
+key = prompt("กรุณาใส่ key หรือพิมพ์ stop");
 }
 console.log('Object:',ectjob);
+
 for (let key in ectjob) {
   alert(`Key: ${key}, Value: ${ectjob[key]}`);
 }
