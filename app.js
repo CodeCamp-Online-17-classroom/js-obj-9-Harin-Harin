0923f53a72b6bbc697e43a4cf58fa70c45bb5dce
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-15 20:24:56 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+let ectjob = {};
+while(true){
+let key = prompt("กรุณาใส่ key หรือพิมพ์ stop");
+if (key.toLowerCase() === 'stop') {
+  break;
+}
+let value = prompt(`กรุณาใส่ value ของ ${key}`);
+ectjob[key] = value;
+}
+console.log('Object:',ectjob);
